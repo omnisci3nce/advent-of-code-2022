7878b178a24490ad7556ad046f4133272b1dd0b2
Omniscient <omniscient.oce@gmail.com>
2023-10-25 22:54:13 +1100

    some changes I forgot to push

--- days/day3.ml
+++ days/day3.ml
@@ -1,77 +1,60 @@
-let item_types = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+open Containers
 
-(** association list of item type -> priority *)
-let priorities =
-  let index = ref 0 in
-  String.fold_left (fun l c ->
-    index := !index + 1;
-    ((String.make 1 c), !index) :: l
-  ) [] item_types
+(* integer priority of each letter from a..zA..Z *)
+let get_priority c =
+  let offset_from base c = CCChar.to_int c - CCChar.to_int base in
+  match c with
+    | 'a' .. 'z' -> 1 + offset_from 'a' c
+    | 'A' .. 'Z' -> 1 + offset_from 'A' c
+    | _ -> failwith "cooked"
 
+module CharSet = Set.Make(Char)
 
+(* kinda scuffed *)
 let parse_rucksack line =
   let compartment_size = (String.length line) / 2 in
-  let c1 = ref ""
-  and c2 = ref "" in
-  String.iteri (fun i c ->
-    if i < compartment_size then
-      c1 := !c1 ^ ( String.make 1 c)
-    else 
-      c2 := !c2 ^ (String.make 1 c)
-    ) line;
-  (!c1, !c2)
+  let char_list = String.to_list line in
+  let rucksack1 = List.take compartment_size char_list |> String.of_list in
+  let rucksack2 = List.last compartment_size char_list |> String.of_list in
+  (rucksack1, rucksack2)
 
-let find_shared_item rucksack =
-  let c1 = Hashtbl.create 8
-  and c2 = Hashtbl.create 8 in
-  let argh tbl s =
-    String.iter (fun c -> Hashtbl.add tbl (String.make 1 c) 1) s
-  in
-  argh c1 (fst rucksack);
-  argh c2 (snd rucksack);
-  let keys = Hashtbl.to_seq_keys c1 in
-  match Seq.find (fun k -> Option.is_some (Hashtbl.find_opt c2 k)) keys with
-  | Some s -> s
-  | _ -> failwith ""
+(* string -> CharSet.t *)
+let charset s = String.to_seq s |> CharSet.of_seq
 
-let find_badge (r1, r2, r3) =
-  let r1_items = Hashtbl.create 8
-  and r2_items = Hashtbl.create 8
-  and r3_items = Hashtbl.create 8 in
-  let argh tbl s =
-    String.iter (fun c -> Hashtbl.add tbl (String.make 1 c) 1) s
-  in
-  argh r1_items r1;
-  argh r2_items r2;
-  argh r3_items r3;
-  let keys = Hashtbl.to_seq_keys r1_items in
-  match Seq.find (fun k ->
-    Option.is_some (Hashtbl.find_opt r2_items k)
-    &&
-    Option.is_some (Hashtbl.find_opt r3_items k)
-  ) keys with
-  | Some badge -> badge
-  | _ -> failwith ""
+(* string list -> char *)
+let find_badge n_rucksacks = 
+  let charsets = List.map charset n_rucksacks in
+  let intersection = List.fold_left CharSet.inter CharSet.empty charsets in
+  CharSet.choose intersection
 
-let flip f x y = f y x
+(* only slightly different to the above. could actually just combine them but fk it *)
+let find_duplicate (r1, r2) = (charset r1, charset r2)
+                              |> (fun (chars1, chars2) -> CharSet.inter chars1 chars2)
+                              |> CharSet.choose
+
 let sum = List.fold_left (+) 0
+
 let () =
-  let contents = In_channel.with_open_bin "days/input_day3.txt" In_channel.input_all in
-  let lines = String.split_on_char '\n' contents in
+  let lines =
+    In_channel.with_open_bin "days/input_day3_sample.txt" In_channel.input_all
+    |> String.split_on_char '\n' in
   let rucksacks = List.map parse_rucksack lines in
+  
+  let duplicates: char list = List.map find_duplicate rucksacks in
+  List.iter (fun d -> Printf.printf "Char duplicate item %c\n" d) duplicates;
+
   List.iter (fun r -> Printf.printf "%s %s\n" (fst r) (snd r)) rucksacks;
 
   let items_sum = rucksacks
-    |> List.map find_shared_item
-    |> List.map ((flip List.assoc) priorities)
+    |> List.map find_duplicate
+    |> List.map get_priority
     |> sum in
   Printf.printf "(part 1) sum: %d\n" items_sum;
 
   (* Part 2 *)
-  let groups = CCList.chunks 3 lines in
-  (* each line of the group is one rucksack *)
+  let groups = List.chunks 3 lines in
   let priority_sum = groups
-    |> List.map (fun g -> find_badge ((List.nth g 0),(List.nth g 1),(List.nth g 2)))
-    |> List.map ((flip List.assoc) priorities)
+    |> List.map find_badge
+    |> List.map get_priority
     |> sum in
   Printf.printf "(part 2) sum: %d\n" priority_sum
